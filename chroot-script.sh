75b4273e03482a7a87674529f7c90614edac650c
Battaglino, Santiago <battas5+mdt@medtronic.com>
2025-07-16 23:52:35 -0300

    fix4

--- chroot-script.sh
+++ chroot-script.sh
@@ -67,3 +67,4 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo ">>> (Chroot) Script finished. Exiting chroot."
+exit
---
+++ step8.sh
@@ -0,0 +1,25 @@
+# Clean up the script
+rm /mnt/root/chroot-script.sh
+
+echo ">>> [8/8] Cloning System A to System B and finalizing GRUB..."
+umount -R /mnt
+# Close the LUKS container before cloning
+cryptsetup close home
+
+dd if=${SYS_A_PART} of=${SYS_B_PART} bs=4M status=progress
+
+# Remount to update GRUB for both slots
+mount ${SYS_A_PART} /mnt
+mount ${EFI_PART} /mnt/boot
+mkdir /mnt/tmp_b
+mount ${SYS_B_PART} /mnt/tmp_b
+
+# Enable os-prober and regenerate GRUB config to see both slots
+echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# Cleanup
+umount -R /mnt
+
+echo "✅ Installation Complete! You can now reboot."
+echo "On boot, you will be prompted for your LUKS password to unlock your home directory."
