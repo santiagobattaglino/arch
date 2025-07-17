cdcc2f37491fae320ed17f5af10c87a15b968a36
Battaglino, Santiago <battas5+mdt@medtronic.com>
2025-07-17 00:10:37 -0300

    fix6

--- step8.sh
+++ step8.sh
@@ -1,3 +1,8 @@
+# Assign partition variables
+EFI_PART="${SSD_DRIVE}1"
+SYS_A_PART="${SSD_DRIVE}2"
+SYS_B_PART="${SSD_DRIVE}3"
+
 # Clean up the script
 rm /mnt/root/chroot-script.sh
 
@@ -6,13 +11,13 @@ umount -R /mnt
 # Close the LUKS container before cloning
 cryptsetup close home
 
-dd if=${SYS_A_PART} of=${SYS_B_PART} bs=4M status=progress
+dd if="${SYS_A_PART}" of="${SYS_B_PART}" bs=4M status=progress
 
 # Remount to update GRUB for both slots
-mount ${SYS_A_PART} /mnt
-mount ${EFI_PART} /mnt/boot
+mount "${SYS_A_PART}" /mnt
+mount "${EFI_PART}" /mnt/boot
 mkdir /mnt/tmp_b
-mount ${SYS_B_PART} /mnt/tmp_b
+mount "${SYS_B_PART}" /mnt/tmp_b
 
 # Enable os-prober and regenerate GRUB config to see both slots
 echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
